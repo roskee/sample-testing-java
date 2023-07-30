ca5249e3733214c54d64fd1472f83a1d687d8ea0
Kirubel Adamu <kirubeladamu@gmail.com>
2023-07-30 16:32:36 +0300

    updated api tests

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,19 +1,34 @@
 import org.junit.Test;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class ApiTest {
     @Test
-    public void testRandomUser() {
+    public void testGetUser() {
         given()
                 .when()
-                .get("https://randomuser.me/api/")
+                .get("https://reqres.in/api/users/1")
                 .then()
-                    .statusCode(200)
-                .body("results", hasItem(
-                        allOf(
-                        hasKey("gender"),hasKey("name"), hasKey("name")
-                        )));
+                .statusCode(200)
+                .body("data.id", equalTo(1))
+                .body("data.email", equalTo("george.bluth@reqres.in"))
+                .body("data.first_name", equalTo("George"))
+                .body("data.last_name", equalTo("Bluth"));
+
+    }
+
+    @Test
+    public void testGetManyUsers() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users?page=1")
+                .then()
+                .statusCode(200)
+                .body("page", equalTo(1))
+                .body("per_page", equalTo(6))
+                .body("total", equalTo(12))
+                .body("data", hasSize(6));
     }
 }
